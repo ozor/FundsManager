90a4d6f8a5ca50d73a278d023b2696394d9feb82
ozor <ozor@yandex.ru>
2023-08-26 23:58:33 +0500

    Add User and stuff for log in

--- symfony-pgsql/src/Controller/SecurityController.php
+++ symfony-pgsql/src/Controller/SecurityController.php
@@ -5,19 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // Логика для страницы входа
-    }
-
-    #[Route('/login_check', name: 'app_login_check')]
-    public function loginCheck()
-    {
         // Логика для проверки данных входа
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
     }
 
     #[Route('/logout', name: 'app_logout')]
