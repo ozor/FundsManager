ed6f9fcd289d5cd91133ba2544c57d83cdae39da
ozor <ozor@yandex.ru>
2023-08-30 23:51:56 +0500

    Update LoginFormAuthenticator class

--- symfony-pgsql/src/Security/LoginFormAuthenticator.php
+++ symfony-pgsql/src/Security/LoginFormAuthenticator.php
@@ -5,15 +5,14 @@ namespace App\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
-class LoginFormAuthenticator extends AbstractAuthenticator
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
-    public function supports(Request $request): ?bool
+    protected function getLoginUrl(Request $request): string
     {
-        // TODO: Implement supports() method.
+        // TODO: Implement getLoginUrl() method.
     }
 
     public function authenticate(Request $request): Passport
@@ -26,19 +25,4 @@ class LoginFormAuthenticator extends AbstractAuthenticator
         // TODO: Implement onAuthenticationSuccess() method.
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    {
-        // TODO: Implement onAuthenticationFailure() method.
-    }
-
-//    public function start(Request $request, AuthenticationException $authException = null): Response
-//    {
-//        /*
-//         * If you would like this class to control what happens when an anonymous user accesses a
-//         * protected page (e.g. redirect to /login), uncomment this method and make this class
-//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
-//         *
-//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
-//         */
-//    }
 }
