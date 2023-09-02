3df92032009319f64dca5c3e7d878fd006210ce0
ozor <ozor@yandex.ru>
2023-09-02 23:52:53 +0500

    Update UserFixtures class

--- symfony-pgsql/src/DataFixtures/UserFixtures.php
+++ symfony-pgsql/src/DataFixtures/UserFixtures.php
@@ -5,15 +5,21 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
         $user = new User();
         $user
             ->setEmail('user@fundsmanager.local')
-            ->setPassword(12345)
+            ->setPassword($this->passwordHasher->hashPassword($user, 12345))
             ->setRoles(['ROLE_USER'])
         ;
 
