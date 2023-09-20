d199b3a4ad69e018d3cd8a3f7d73a68d988dd372
ozor <ozor@yandex.ru>
2023-09-20 23:56:35 +0500

    Update Home Controller

--- symfony-pgsql/src/Controller/Cabinet/HomeController.php
+++ symfony-pgsql/src/Controller/Cabinet/HomeController.php
@@ -5,7 +5,9 @@ namespace App\Controller\Cabinet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class HomeController extends AbstractController
 {
     #[Route('/cabinet', name: 'app_cabinet_home')]
