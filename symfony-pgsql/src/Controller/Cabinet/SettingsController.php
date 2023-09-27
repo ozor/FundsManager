10cd815eef69c9c0ffe963725beb593789acece8
ozor <ozor@yandex.ru>
2023-09-27 23:34:59 +0500

    Update Settings Controller

--- symfony-pgsql/src/Controller/Cabinet/SettingsController.php
+++ symfony-pgsql/src/Controller/Cabinet/SettingsController.php
@@ -2,13 +2,21 @@
 
 namespace App\Controller\Cabinet;
 
+use App\Entity\User;
+use App\Form\ChangePasswordFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/cabinet/settings')]
+#[IsGranted('ROLE_USER')]
 class SettingsController extends AbstractController
 {
-    #[Route('/settings', name: 'app_cabinet_settings')]
+    #[Route('/', name: 'app_cabinet_settings', methods: ['GET', 'POST'])]
     public function index(): Response
     {
         return $this->render('cabinet/settings/index.html.twig', [
@@ -16,11 +24,38 @@ class SettingsController extends AbstractController
         ]);
     }
 
-    #[Route('/settings/password', name: 'app_cabinet_settings_password')]
-    public function password(): Response
-    {
+    #[Route('/password', name: 'app_cabinet_settings_password', methods: ['GET', 'POST'])]
+    public function password(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager,
+    ): Response {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(ChangePasswordFormType::class, null, [
+            'action' => $this->generateUrl('app_cabinet_settings_password'),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$passwordHasher->isPasswordValid($user, $form->get('currentPassword')->getData())) {
+                $this->addFlash('error', 'Current password is invalid');
+                return $this->redirectToRoute('app_cabinet_settings_password');
+            }
+
+            $hashedPassword = $passwordHasher->hashPassword($user, $form->get('newPassword')->getData());
+            $user->setPassword($hashedPassword);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_cabinet_settings_password');
+        }
+
         return $this->render('cabinet/settings/password.html.twig', [
-            'controller_name' => 'SettingsController',
+            'form' => $form->createView(),
         ]);
     }
 }
