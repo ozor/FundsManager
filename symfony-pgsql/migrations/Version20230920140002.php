f4832f479c87f39b2b77b9981398cda767d8a507
ozor <ozor@yandex.ru>
2023-09-27 23:35:22 +0500

    Update migration

--- symfony-pgsql/migrations/Version20230920140002.php
+++ symfony-pgsql/migrations/Version20230920140002.php
@@ -10,17 +10,18 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230826185344 extends AbstractMigration
+final class Version20230920140002 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Create tables for user and messenger';
+        return 'Add `users` table';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql('CREATE SEQUENCE "users_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE "users" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON "users" (email)');
         $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
         $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
@@ -40,7 +41,8 @@ final class Version20230826185344 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP SEQUENCE "users_id_seq" CASCADE');
+        $this->addSql('DROP TABLE "users"');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
